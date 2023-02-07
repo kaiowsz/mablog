d3b48934f50cfa7f91e2990973b29df85405fb38
Kaio Silveira <kaionpsa@gmail.com>
2023-02-07 08:50:04 -0300

    folder structure

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Head() {
+  return (
+    <>
+      <title>Mablog</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/vercel.svg" />
+    </>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1 className="text-4xl">Home Page</h1>
+        <p>page</p>
+    </div>
+  )
+}
+
+export default Home
